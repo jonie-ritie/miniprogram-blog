bc5a5ad1a3a7dae2dc39ce78f2e31688081ab132
YL-PC-0101\Xforce <lujiannan@xforceplus.com>
2022-07-28 17:19:25 +0800

    update

--- pages/index/index.js
+++ pages/index/index.js
@@ -172,6 +172,32 @@ Page({
       postsList: errlist
     })
   },
+  // 获取滚动条当前位置
+  onPageScroll: function (e) {
+    console.log(e)
+    if (e.scrollTop > 200) {
+      this.setData({
+        floorstatus: true
+      });
+    } else {
+      this.setData({
+        floorstatus: false
+      });
+    }
+  },
+  //回到顶部
+  goTop: function (e) {  // 一键回到顶部
+    if (wx.pageScrollTo) {
+      wx.pageScrollTo({
+        scrollTop: 0
+      })
+    } else {
+      wx.showModal({
+        title: '提示',
+        content: '当前微信版本过低，无法使用该功能，请升级到最新微信版本后重试。'
+      })
+    }
+  },
 
   // },
   // //首页图标跳转
