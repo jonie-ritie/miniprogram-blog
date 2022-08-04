f855b3ea585631b0ffcc9d9adb6f9e986b14da55
YL-PC-0101\Xforce <lujiannan@xforceplus.com>
2022-08-04 18:12:58 +0800

    update

--- pages/main/main.js
+++ pages/main/main.js
@@ -17,30 +17,111 @@ Page({
     hasUserInfo: false,
     closeLoginBlock: false,
     isLogin: false,
+    isLoaded: true,
+    weatherTop: 83,
+    weatherHeight: 32,
+    weatherWidth: 87,
+    region: [],
+    temperature: "~°C",
+    weatherIcon: "../../static/weather/999-fill.svg",
+    addrid: 0,
+    longitude: 0,
+    latitude: 0,
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    // 设置天气胶囊所在位置
+    this.setWeatherCellStyle();
+    
+    //设置用户所在位置
+    this.setUserLoc();
+
+    this.getSelectLoc();
+  },
+  setUserLoc: function(e) {
+    var that = this;
+    wx.getFuzzyLocation({
+      type: 'wgs84',
+      success (res) {
+        that.setData({
+          latitude: res.latitude,
+          longitude: res.longitude,
+        })
+      }
+    })
+  },
+  setWeatherCellStyle: function () {
+    // 设置天气胶囊所在位置
+    this.setData({
+      weatherTop: wx.getMenuButtonBoundingClientRect().top + wx.getMenuButtonBoundingClientRect().height,
+      weatherHeight: wx.getMenuButtonBoundingClientRect().height,
+      weatherWidth: wx.getMenuButtonBoundingClientRect().width,
+    })
+  },
+  getSelectLoc: function() {
+    var that = this; //this不可以直接在wxAPI函数内部使用
+    // 调用和风天气API获取位置ID
+    wx.request({
+      url: 'https://geoapi.heweather.net/v2/city/lookup?',
+      data: {
+        location: that.data.longitude + "," + that.data.latitude,
+        key: 'cdf6c29c017144a49986d73463f868ca'
+      },
+      success: function (res) {
+        console.log(res.data)
+        that.setData({ addrid: res.data.location[0].id })
+      },
+      complete: function () {
+        if (that.data.addrid !== 0) {
+          that.getWeather();
+        }
+      }
+    });
+  },
+  getWeather: function () {
+    var that = this; //this不可以直接在wxAPI函数内部使用 
+    // 调用和风天气API获取天气信息
+    wx.request({
+      url: 'https://devapi.heweather.net/v7/weather/now?',
+      data: {
+        location: that.data.addrid,
+        key: 'cdf6c29c017144a49986d73463f868ca'
+      },
+      success: function (res) {
+        console.log(res.data)
+        that.setData({ 
+          temperature: res.data.now.temp + "°C",
+          weatherIcon: "../../static/weather/" + res.data.now.icon + "-fill.svg",
+        })
+      }
+    });
   },
   // 登录
   getUserProfile(e) {
+    this.setData({isLoaded: false});
     // 推荐使用wx.getUserProfile获取用户信息，开发者每次通过该接口获取用户个人信息均需用户确认
     // 开发者妥善保管用户快速填写的头像昵称，避免重复弹窗
     wx.getUserProfile({
-      desc: '用于完善会员资料', // 声明获取用户个人信息后的用途，后续会展示在弹窗中，请谨慎填写
+      desc: '收集小程序质量以及错误反馈', // 声明获取用户个人信息后的用途，后续会展示在弹窗中，请谨慎填写
       success: (res) => {
         wx.setStorageSync("userInfo", res.userInfo);
         this.setData({
           hasUserInfo: res.userInfo,
           isLogin: true,
           closeLoginBlock: true,
-        })
+        });
         wx.showToast({
           title: '更新成功',
           icon: 'none',
           duration: 1000
+        });
+      },
+      complete: (res) => {
+        this.setData({
+          isLoaded: true,
         });
       }
     })
